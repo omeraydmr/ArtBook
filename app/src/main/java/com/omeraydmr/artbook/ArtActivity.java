331e5ad391978301e6a62f9b0e65b4396ab7d51c
Ömer Aydemir <osilix1@gmail.com>
2021-09-02 14:34:26 +0300

    Control of permission is added.

--- app/src/main/java/com/omeraydmr/artbook/ArtActivity.java
+++ app/src/main/java/com/omeraydmr/artbook/ArtActivity.java
@@ -1,10 +1,15 @@
 package com.omeraydmr.artbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.omeraydmr.artbook.databinding.ActivityArtBinding;
 import com.omeraydmr.artbook.databinding.ActivityMainBinding;
 
@@ -26,5 +31,25 @@ public class ArtActivity extends AppCompatActivity {
 
     public void selectImage(View view) {
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                //snackbar creating
+                Snackbar.make(view, "Permission Needed for gallery.", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).show();
+            }
+            else {
+                //request permission
+            }
+
+
+        }
+        else {
+
+        }
     }
 }
