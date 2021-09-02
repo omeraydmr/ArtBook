d3cc731d3346b7d8adeee941cfafd7a860f02679
Ömer Aydemir <osilix1@gmail.com>
2021-09-02 13:56:20 +0300

    The Menu bar created.

--- app/src/main/java/com/omeraydmr/artbook/ArtActivity.java
+++ app/src/main/java/com/omeraydmr/artbook/ArtActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.omeraydmr.artbook.databinding.ActivityArtBinding;
+import com.omeraydmr.artbook.databinding.ActivityMainBinding;
+
 public class ArtActivity extends AppCompatActivity {
+
+    private ActivityArtBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art);
+        binding = ActivityArtBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void save(View view) {
--- app/src/main/java/com/omeraydmr/artbook/MainActivity.java
+++ app/src/main/java/com/omeraydmr/artbook/MainActivity.java
@@ -1,14 +1,50 @@
 package com.omeraydmr.artbook;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.omeraydmr.artbook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item .getItemId() == R.id.add_art) {
+            Intent intent = new Intent(this, ArtActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
